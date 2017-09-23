c4349c1ce948e6ad8ee6a172447218178050b1a3
Edge <baiban1994@gmail.com>
2017-09-23 16:02:17 +0800

    使用webpack自带的方式导入第三方包，但是我感觉是没有成功的
    需要在js文件导入没有的变量

--- webpack-multipage/src/js/index.js
+++ webpack-multipage/src/js/index.js
@@ -1,8 +1,4 @@
 import './../css/index.css';
-import $ from 'jquery'
 require(['./common.js'],(common)=>{
 	common.initIndex();
-	$(function(){
-		console.log(`this is jquery`);
-	})
 });
--- webpack-multipage/webpack.config.js
+++ webpack-multipage/webpack.config.js
@@ -2,9 +2,11 @@ var path = require('path');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 var cleanWebpack = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
 	entry:{
+		vender:['jquery','./src/js/common.js'],
 		index:'./src/js/index.js',
 		cart:'./src/js/cart.js',
 	},
@@ -39,17 +41,22 @@ module.exports = {
 		]
 	},
 	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name:'vender',
+			chunks:['index','cart','vender'],
+			mikChunks:3
+		}),
 		new htmlWebpackPlugin({
 			filename:'index-1.html',
 			title:'Home',
 			template:'./src/index.html',
-			chunks:['index']
+			chunks:['index','vender']
 		}),
 		new htmlWebpackPlugin({
 			filename:'cart-1.html',
 			title:'Cart',
 			template:'./src/cart.html',
-			chunks:['cart']
+			chunks:['cart','vender']
 		}),
 		new cleanWebpack(['./dist'],{
 			root: '',
@@ -57,6 +64,7 @@ module.exports = {
       dry: false
 		}),
 		new ExtractTextPlugin('main.css')
+		
 	]
 	// devtool:"source-map"
 }
